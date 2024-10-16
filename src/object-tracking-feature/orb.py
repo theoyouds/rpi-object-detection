6440dea55b8ddd3b7abcaeafe8a58344b85d422f
theoyouds <theo@youds.me>
2024-10-16 16:47:04 +0100

    Trying out feature tracking for glasses case.

--- src/object-tracking-feature/orb.py
+++ src/object-tracking-feature/orb.py
@@ -7,7 +7,7 @@
 # Twitter: @yfrobotics
 # Website: https://www.yfrl.org
 # ------------------------------------------------------------------------------
-# References: 
+# References:
 # - https://circuitdigest.com/tutorial/real-life-object-detection-using-opencv-python-detecting-objects-in-live-video
 # - https://docs.opencv.org/4.x/d1/d89/tutorial_py_orb.html
 # ------------------------------------------------------------------------------
@@ -15,6 +15,9 @@
 import cv2
 import numpy as np
 import time
+
+from picamera2 import Picamera2
+
 
 def ORB_detector(new_image, image_template):
     # Function that compares input image to template
@@ -30,11 +33,11 @@ def ORB_detector(new_image, image_template):
     # Detect keypoints of rotated image
     (kp2, des2) = orb.detectAndCompute(image_template, None)
 
-    # Create matcher 
+    # Create matcher
     # Note we're no longer using Flannbased matching
     bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
     # Do matching
-    matches = bf.match(des1,des2)
+    matches = bf.match(des1, des2)
 
     # Sort the matches based on distance.  Least distance
     # is better
@@ -54,10 +57,17 @@ def visualize_fps(image, fps: int):
     font_thickness = 1
 
     # Draw the FPS counter
-    fps_text = 'FPS = {:.1f}'.format(fps)
+    fps_text = "FPS = {:.1f}".format(fps)
     text_location = (left_margin, row_size)
-    cv2.putText(image, fps_text, text_location, cv2.FONT_HERSHEY_PLAIN,
-                font_size, text_color, font_thickness)
+    cv2.putText(
+        image,
+        fps_text,
+        text_location,
+        cv2.FONT_HERSHEY_PLAIN,
+        font_size,
+        text_color,
+        font_thickness,
+    )
 
     return image
 
@@ -67,14 +77,18 @@ fps = 0
 cap = cv2.VideoCapture(0)
 
 # Load our image template, this is our reference image
-image_template = cv2.imread('simple.png', 0) 
+image_template = cv2.imread("simple.png", 0)
 
 while True:
     # ----------------------------------------------------------------------
     # record start time
     start_time = time.time()
     # Get webcam images
-    ret, frame = cap.read()
+    picam2 = Picamera2()
+    picam2.start()
+
+    frame = picam2.capture_array()
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
     # Get height and width of webcam frame
     height, width = frame.shape[:2]
@@ -86,32 +100,44 @@ while True:
     bottom_right_y = int((height / 2) - (height / 4))
 
     # Draw rectangular window for our region of interest
-    cv2.rectangle(frame, (top_left_x,top_left_y), (bottom_right_x,bottom_right_y), 255, 3)
+    cv2.rectangle(
+        frame, (top_left_x, top_left_y), (bottom_right_x, bottom_right_y), 255, 3
+    )
 
     # Crop window of observation we defined above
-    cropped = frame[bottom_right_y:top_left_y , top_left_x:bottom_right_x]
+    cropped = frame[bottom_right_y:top_left_y, top_left_x:bottom_right_x]
 
     # Flip frame orientation horizontally
-    frame = cv2.flip(frame,1)
+    frame = cv2.flip(frame, 1)
 
-    # Get number of ORB matches 
+    # Get number of ORB matches
     matches = ORB_detector(cropped, image_template)
 
-    # Display status string showing the current no. of matches 
+    # Display status string showing the current no. of matches
     output_string = "# of Matches = " + str(matches)
-    cv2.putText(frame, output_string, (50,450), cv2.FONT_HERSHEY_COMPLEX, 1, (250,0,0), 2)
+    cv2.putText(
+        frame, output_string, (50, 450), cv2.FONT_HERSHEY_COMPLEX, 1, (250, 0, 0), 2
+    )
 
     # Our threshold to indicate object deteciton
-    # For new images or lightening conditions you may need to experiment a bit 
+    # For new images or lightening conditions you may need to experiment a bit
     # Note: The ORB detector to get the top 1000 matches, 350 is essentially a min 35% match
     threshold = 200
 
     # If matches exceed our threshold then object has been detected
     if matches > threshold:
-        cv2.rectangle(frame, (top_left_x,top_left_y), (bottom_right_x,bottom_right_y), (0,255,0), 3)
-        cv2.putText(frame,'Object Found',(50,50), cv2.FONT_HERSHEY_COMPLEX, 2 ,(0,255,0), 2)
+        cv2.rectangle(
+            frame,
+            (top_left_x, top_left_y),
+            (bottom_right_x, bottom_right_y),
+            (0, 255, 0),
+            3,
+        )
+        cv2.putText(
+            frame, "Object Found", (50, 50), cv2.FONT_HERSHEY_COMPLEX, 2, (0, 255, 0), 2
+        )
 
-    cv2.imshow('Object Detector using ORB', visualize_fps(frame, fps))
+    cv2.imshow("Object Detector using ORB", visualize_fps(frame, fps))
     # ----------------------------------------------------------------------
     # record end time
     end_time = time.time()
@@ -119,7 +145,7 @@ while True:
     seconds = end_time - start_time
     fps = 1.0 / seconds
     print("Estimated fps:{0:0.1f}".format(fps))
-    if cv2.waitKey(1) == 13: #13 is the Enter Key
+    if cv2.waitKey(1) == 13:  # 13 is the Enter Key
         break
 
 cap.release()
