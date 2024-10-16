cf04f458b4782aaff3c882f567857bc21d2664a6
theoyouds <theo@youds.me>
2024-10-16 16:15:03 +0100

    Setup shape tracking for picam2.

--- src/object-tracking-shape/cv_object_tracking_shape.py
+++ src/object-tracking-shape/cv_object_tracking_shape.py
@@ -9,7 +9,7 @@
 # Twitter: @yfrobotics
 # Website: https://www.yfrl.org
 # ------------------------------------------------------------------------------
-# Reference: 
+# Reference:
 # - https://www.pyimagesearch.com/2014/07/21/detecting-circles-images-using-opencv-hough-circles/
 # ------------------------------------------------------------------------------
 
@@ -17,14 +17,17 @@ import cv2
 import numpy as np
 import time
 
+from picamera2 import Picamera2
+
 CAMERA_DEVICE_ID = 0
 IMAGE_WIDTH = 320
 IMAGE_HEIGHT = 240
 fps = 0
 
+
 def isset(v):
     try:
-        type (eval(v))
+        type(eval(v))
     except:
         return 0
     else:
@@ -43,20 +46,31 @@ def visualize_fps(image, fps: int):
     font_thickness = 1
 
     # Draw the FPS counter
-    fps_text = 'FPS = {:.1f}'.format(fps)
+    fps_text = "FPS = {:.1f}".format(fps)
     text_location = (left_margin, row_size)
-    cv2.putText(image, fps_text, text_location, cv2.FONT_HERSHEY_PLAIN,
-                font_size, text_color, font_thickness)
+    cv2.putText(
+        image,
+        fps_text,
+        text_location,
+        cv2.FONT_HERSHEY_PLAIN,
+        font_size,
+        text_color,
+        font_thickness,
+    )
 
     return image
 
 
 if __name__ == "__main__":
     try:
+
+        picam2 = Picamera2()
+        picam2.start()
+
         # create video capture
         cap = cv2.VideoCapture(CAMERA_DEVICE_ID)
 
-        # set resolution to 320x240 to reduce latency 
+        # set resolution to 320x240 to reduce latency
         cap.set(3, IMAGE_WIDTH)
         cap.set(4, IMAGE_HEIGHT)
 
@@ -65,32 +79,35 @@ if __name__ == "__main__":
             # record start time
             start_time = time.time()
             # Read the frames frome a camera
-            _, frame = cap.read()
-            frame = cv2.blur(frame,(3,3))
+            frame = picam2.capture_array()
+
+            frame = cv2.blur(frame, (3, 3))
 
             # Or get it from a JPEG
             # frame = cv2.imread('frame0010.jpg', 1)
-        
+
             # convert the image into gray color
             output = frame.copy()
             gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
             # detect circles in the image
             circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1.2, 100)
-            
+
             # ensure at least some circles were found
             if circles is not None:
                 # convert the (x, y) coordinates and radius of the circles to integers
                 circles = np.round(circles[0, :]).astype("int")
                 # loop over the (x, y) coordinates and radius of the circles
-                for (x, y, r) in circles:
+                for x, y, r in circles:
                     # draw the circle in the output image, then draw a rectangle
                     # corresponding to the center of the circle
                     cv2.circle(output, (x, y), r, (0, 255, 0), 4)
-                    cv2.rectangle(output, (x - 5, y - 5), (x + 5, y + 5), (0, 128, 255), -1)
-            
+                    cv2.rectangle(
+                        output, (x - 5, y - 5), (x + 5, y + 5), (0, 128, 255), -1
+                    )
+
             # show the output image
-            cv2.imshow('img', np.hstack([frame, output]))
+            cv2.imshow("img", np.hstack([frame, output]))
             # cv2.imshow("frame", np.hstack([visualize_fps(frame, fps), visualize_fps(output, fps)]))
             # ----------------------------------------------------------------------
             # record end time
